8c1fe6e256ca1ce88553da32ef026bc5b54dfa43
Dashiel Lopez Mendez <hi@64f.dev>
2024-12-28 12:06:33 -0500

    Address lint issue

--- app/src/bin/operator.rs
+++ app/src/bin/operator.rs
@@ -1,9 +1,9 @@
-use cloudsecrets::CloudSecret;
-use kube::Client;
+// use cloudsecrets::CloudSecret;
+// use kube::Client;
 
 #[tokio::main]
 async fn main() {
-    let client = Client::try_default()
-        .await
-        .expect("Failed to create Kubernetes client");
+    // let client = Client::try_default()
+    //     .await
+    //     .expect("Failed to create Kubernetes client");
 }
